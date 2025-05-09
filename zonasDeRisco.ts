5e05ca5173a5c710779c7886cd65b359a4e164f6
joaovitorevora <joaovitorevora@gmail.com>
2025-05-09 16:57:54 -0300

    update icons

--- zonasDeRisco.ts
+++ zonasDeRisco.ts
@@ -4,20 +4,27 @@ export const zonasDeRisco = [
     latitude: -22.595681,
     longitude: -47.408866,
     raio: 50,
-    tipo: 'furto' // já está ok
+    tipo: 'furto'
   },
   {
     id: 'zona2',
     latitude: -22.591636,
     longitude: -47.406538,
     raio: 100,
-    tipo: 'assalto' // aqui estava 'Assalto'
+    tipo: 'assalto' 
   },
   {
     id: 'zona3',
     latitude: -22.594875,
     longitude: -47.407193,
     raio: 100,
-    tipo: 'homicídio' // aqui estava 'Homicídio'
+    tipo: 'homicídio' 
+  },
+  {
+    id: 'zona4',
+    latitude: -22.534323,
+    longitude: -47.384065,
+    raio: 100,
+    tipo: 'homicídio' 
   },
 ];
